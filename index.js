e83b02484f9d4b6475d4352e1ccf81c0ab063821
Daniel Chappell <daniel.chappell@q2ebanking.com>
2017-10-28 13:25:16 -0500

    initial index commit

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var merge = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-cli-maybe-not'
+  name: 'ember-cli-maybe-not',
+    treeForAddon: function(tree) {
+        var maybeNotTree = path.dirname(require.resolve('maybe-not'));
+        var trees = tree ? merge([maybeNotTree, tree]) : maybeNotTree;
+
+        return this._super.treeForAddon.call(this, trees);
+    }
 };
